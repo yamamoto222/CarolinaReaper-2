3bec70e9c25ce97ea6441127811db671f49df9bd
kitano <kitano@jted001.jig-saw.com>
2016-05-13 17:59:22 +0900

    5th

--- src/model/CreateAndAnnihilateEventPage.java
+++ src/model/CreateAndAnnihilateEventPage.java
@@ -4,13 +4,16 @@ import java.util.Random;
 public class  CreateAndAnnihilateEventPage {
 
 
-	public static String createEventPageFileName(String strId){
-		Random rand=new Random();
+	public static String createEventPageId(String strId){//EventIdからURL用のEventIdに変換。
+		Random rand=new Random();//乱数を用いてURL推定しにくくする
 		return String.valueOf(( rand.nextInt(9999) + 1 )*10000 + Integer.parseInt(strId)+ ".jsp");
 	}
 
-	public static void createEventPageFile(Event event){
-		event.setEventPageFileName(createEventPageFileName(event.getEventId()) + ".jsp");
+	public static void createEventPageUrl(Event event){///EventClassからEventURLを生成。ID生成後に実行すべし
+		String servletUrl ="";//event詳細ページのサーブレットのURL
+		event.setEventUrl(servletUrl + "?pageid=" + createEventPageId(event.getEventId()));
 	}
+
+
 
 }
